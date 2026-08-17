9cff65ed6086fd6fcf4f3ec5ec8a70554a447dd4
Jeff Dickey <jdx@pro.wallaby-spica.ts.net>
2026-08-17 09:25:12 -0500

    add search sitemaps

---
+++ web/src/pages/sitemap.xml.ts
@@ -0,0 +1,39 @@
+import type { APIRoute } from "astro";
+import { env } from "cloudflare:workers";
+import { loadToolsJson } from "../lib/data-loader";
+
+const ORIGIN = "https://mise-versions.jdx.dev";
+
+function escapeXml(value: string): string {
+  return value
+    .replaceAll("&", "&amp;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&apos;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;");
+}
+
+export const GET: APIRoute = async () => {
+  const data = await loadToolsJson(env.ANALYTICS_DB);
+  if (!data) {
+    return new Response("Failed to load tools", { status: 500 });
+  }
+
+  const urls = [
+    `${ORIGIN}/`,
+    `${ORIGIN}/stats`,
+    ...data.tools.map(
+      (tool) => `${ORIGIN}/tools/${encodeURIComponent(tool.name)}`,
+    ),
+  ]
+    .map((url) => `<url><loc>${escapeXml(url)}</loc></url>`)
+    .join("");
+  const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=600, stale-while-revalidate=3600",
+    },
+  });
+};
